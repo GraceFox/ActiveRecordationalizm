7ae1a7c4eac873b18dd7b666ab9219e87eb10bbb
Adam Ron <apprentice@Samoa.local>
2014-08-28 17:33:22 +1200

    Added session functionality

--- app/controllers/controller.rb
+++ app/controllers/controller.rb
@@ -4,9 +4,25 @@ end
 
 get '/:guide_name' do
   @guide = Guide.find_by name: "#{params[:guide_name]}"
-  erb :guide
+  erb :guides
 end
 
 post '/log_in' do
+  user = params["log_in"]
+  @contributor = Contributor.find_by_name(user["user_name"])
+  if @contributor == nil
+    @error = "Your user name isn't recognised here. Also, you're ugly"
+    @error.inspect
+  elsif @contributor.is_password_valid(user["password"])
+      session[:user_id] = @contributor.id
+  else
+      @error = "Your password is wrong. You can do better"
+      @error.inspect
+  end
+  redirect to('/')
 end
 
+post '/log_out' do
+  session[:user_id] = nil
+  redirect to('/')
+end
